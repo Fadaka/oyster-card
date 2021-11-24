57dd58d3af069dd69f80121ebac421108fe83cf5
Fadaka <master_kunle14@hotmail.co.uk>
2021-11-24 16:38:32 +0000

    created station class and added name/zone variables

---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require 'station'
+
+describe Station do
+    let(:station) {described_class.new("Aldgate East", 1)}
+
+
+    it 'has a station name 'do
+    expect(station.name).to eq("Aldgate East")
+    end
+
+    it 'has a station zone 'do
+    expect(station.zone).to be(1)
+    end
+end
