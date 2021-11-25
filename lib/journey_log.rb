9e700d0613c3f202c34e0a52971439aa78245605
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-25 17:20:54 +0000

    Inject journey_class with dependency injection

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,11 +1,20 @@
 class JourneyLog
   attr_reader :journeys
-
-  def initialize
+  
+  def initialize(journey_class: Journey)
     @journeys = []
+    @journey_class = journey_class
+    @current_journey 
   end
 
   def start(station)
     @journeys << station
   end
+a_journey = { entry: 'station', exit: nil }
+
+  private
+  def current_journey
+    @current_journey ||= journey_class.new
+  end
 end
+
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -5,10 +5,15 @@ require 'journey'
 
 describe JourneyLog do
   let(:journey_log) { described_class.new }
-  let(:station) { double :station }
+  let(:station) { double :station}
   
   it 'starts a journey' do
     journey_log.start(station)
     expect(journey_log.journeys.length).to eq(1)
+  end
+
+  it 'finishes a journey' do
+    journey_log.finish(station)
+    expect(journey_log.)
   end
 end
